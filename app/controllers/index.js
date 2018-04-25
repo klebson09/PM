d35ac3062619bb04ded8df45967a8921065640c0
Klebson Leonardo <silvaklebson_@hotmail.com>
2018-04-24 21:24:19 -0300

    jode js configuração de rotas

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,5 @@
 module.exports.home = function(application, req, res){
-	res.render("login/login", {validacao: {}});
+	// res.render("login/login", {validacao: {}});
+	console.log("CONTROLLER BLANK <---");
+	res.render("includes/blank", {validacao: {}});
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 module.exports = function(application){
-    application.get('/login', function(req, res){
+    // application.get('/login', function(req, res){
+    application.get('/includes', function(req, res){
+      console.log("ROUTERS BLANK <---");
       application.app.controllers.index.home(application, req, res);
     });
 }
