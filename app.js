a555d69be2235e03e6fcbe277dcc41582fddbe84
Klebson Leonardo <silvaklebson_@hotmail.com>
2018-04-29 13:55:30 -0300

    Modificação da porta
    de 8085 para 3000 (porta padrão da umbler)

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-var server = app.listen(8085, function(){
+var server = app.listen(3000, function(){
 	console.log('Servidor online');
 })
-
