4cf1c10a575ac0ec6bf6415bf6d4dc7399d5ab79
mlbs1994 <matheuslevibarouh@gmail.com>
2019-09-17 07:00:02 -0300

    Ajustes finais pré teste de usabilidade - Tratamento Exceções Aplicação - PARCIAL

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ var porta = process.env.PORT || 3000;
 // app.listen(porta);
 var server = app.listen(porta, function(){
 	console.log('Servidor online');
-})
+});
+
+process.on("uncaughtException", function(error){
+	console.log("UMA EXCEÇÃO FOI CAPTURADA!!!")
+	console.log("ERRO = "+error)
+});
