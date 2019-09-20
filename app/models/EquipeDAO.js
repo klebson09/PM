f8223d8bd5fd0514bf44925f80efb74442caa8d7
mlbs1994 <matheuslevibarouh@gmail.com>
2019-09-19 23:01:49 -0300

    ajuste EquipeDAO membrosequipe

--- app/models/EquipeDAO.js
+++ app/models/EquipeDAO.js
@@ -1,5 +1,5 @@
 var sqlValidarEquipeTutor = "SELECT * FROM membrosequipe INNER JOIN conta_usuario ON membrosequipe.conta_usuario_idContaUsuario = conta_usuario.idContaUsuario INNER JOIN equipe ON  membrosequipe.equipe_idEquipe = equipe.idEquipe";
-var sqlVerificarUsuarioVinculadoEquipe = "SELECT * FROM membrosEquipe WHERE conta_usuario_idContaUsuario = ";
+var sqlVerificarUsuarioVinculadoEquipe = "SELECT * FROM membrosequipe WHERE conta_usuario_idContaUsuario = ";
 
 function EquipeDAO(connection){
   this._connection = connection();
@@ -23,34 +23,34 @@ EquipeDAO.prototype.cadEquipe = function(equipe, idAdmEqp ,callback){
     this._connection.query(sqlInsertEquipe, callback)
 }
 
-EquipeDAO.prototype.cadMembrosEquipe = function(membrosEquipe, idEquipe, callback){
+EquipeDAO.prototype.cadmembrosequipe = function(membrosequipe, idEquipe, callback){
   console.log("Inserindo membros na equipe");
 
-  var sqlInsertMembrosEquipe = "INSERT INTO membrosequipe (conta_usuario_idContaUsuario, equipe_idEquipe) VALUES (";
+  var sqlInsertmembrosequipe = "INSERT INTO membrosequipe (conta_usuario_idContaUsuario, equipe_idEquipe) VALUES (";
 
   var i=0;
 
-  for(i=0;i<membrosEquipe.length;i++){
-    if(i!=(membrosEquipe.length-1)){
-      sqlInsertMembrosEquipe += "'"+membrosEquipe[i]+"', '"+idEquipe+"'), (";
+  for(i=0;i<membrosequipe.length;i++){
+    if(i!=(membrosequipe.length-1)){
+      sqlInsertmembrosequipe += "'"+membrosequipe[i]+"', '"+idEquipe+"'), (";
     } else {
-      sqlInsertMembrosEquipe += "'"+membrosEquipe[i]+"', '"+idEquipe+"')";
+      sqlInsertmembrosequipe += "'"+membrosequipe[i]+"', '"+idEquipe+"')";
     }
 
   }
 
-  console.log(sqlInsertMembrosEquipe);
+  console.log(sqlInsertmembrosequipe);
 
-  this._connection.query(sqlInsertMembrosEquipe, callback)
+  this._connection.query(sqlInsertmembrosequipe, callback)
 }
 
 EquipeDAO.prototype.validarEquipeTutor = function(idEquipe, callback){
-   sqlValidarEquipeTutor += " WHERE conta_usuario.tipoUsuario = 'T' AND membrosEquipe.equipe_idEquipe ='"+idEquipe+"' AND equipe.status = 'Ativo'";
+   sqlValidarEquipeTutor += " WHERE conta_usuario.tipoUsuario = 'T' AND membrosequipe.equipe_idEquipe ='"+idEquipe+"' AND equipe.status = 'Ativo'";
    this._connection.query(sqlValidarEquipeTutor, callback);
 }
 
 EquipeDAO.prototype.verificarUsuarioVinculadoEquipe = function(idContaUsuario, callback){
-  var sqlVerificarUsuarioVinculadoEquipe = "SELECT me.*, eq.* FROM membrosEquipe me INNER JOIN equipe eq ON me.equipe_IdEquipe = eq.idEquipe WHERE me.conta_usuario_idContaUsuario = "+idContaUsuario;
+  var sqlVerificarUsuarioVinculadoEquipe = "SELECT me.*, eq.* FROM membrosequipe me INNER JOIN equipe eq ON me.equipe_IdEquipe = eq.idEquipe WHERE me.conta_usuario_idContaUsuario = "+idContaUsuario;
    this._connection.query(sqlVerificarUsuarioVinculadoEquipe, callback);
 }
 
@@ -61,24 +61,24 @@ EquipeDAO.prototype.obterDadosEquipe = function(idEquipe, callback){
   this._connection.query(sqlObterDadosEquipe, callback);
 }
 
-EquipeDAO.prototype.obterMembrosEquipe = function(idEquipe, callback){
-  console.log("EquipeDAO:obterMembrosEquipe - INICIO");
-  var sqlObterMembrosEquipe = "SELECT * FROM conta_usuario INNER JOIN membrosequipe ON conta_usuario.idContaUsuario = membrosequipe.conta_usuario_idContaUsuario INNER JOIN projeto ON membrosequipe.equipe_idEquipe = projeto.idEquipe  INNER JOIN equipe ON projeto.idEquipe = equipe.idEquipe WHERE membrosequipe.equipe_idEquipe = '"+idEquipe+"' AND projeto.status != 'Concluído'";   
-  console.log("EquipeDAO:obterMembrosEquipe - sqlObterMembrosEquipe = "+sqlObterMembrosEquipe);
-  this._connection.query(sqlObterMembrosEquipe, callback);
+EquipeDAO.prototype.obtermembrosequipe = function(idEquipe, callback){
+  console.log("EquipeDAO:obtermembrosequipe - INICIO");
+  var sqlObtermembrosequipe = "SELECT * FROM conta_usuario INNER JOIN membrosequipe ON conta_usuario.idContaUsuario = membrosequipe.conta_usuario_idContaUsuario INNER JOIN projeto ON membrosequipe.equipe_idEquipe = projeto.idEquipe  INNER JOIN equipe ON projeto.idEquipe = equipe.idEquipe WHERE membrosequipe.equipe_idEquipe = '"+idEquipe+"' AND projeto.status != 'Concluído'";   
+  console.log("EquipeDAO:obtermembrosequipe - sqlObtermembrosequipe = "+sqlObtermembrosequipe);
+  this._connection.query(sqlObtermembrosequipe, callback);
 }
 
-EquipeDAO.prototype.obterMembrosEquipeTutor = function(idEquipe, callback){
-  console.log("EquipeDAO:obterMembrosEquipeTutor - INICIO");
-  var sqlObterMembrosEquipeTutor = "SELECT * FROM conta_usuario INNER JOIN membrosequipe ON conta_usuario.idContaUsuario = membrosequipe.conta_usuario_idContaUsuario INNER JOIN equipe ON membrosequipe.equipe_idEquipe = equipe.idEquipe WHERE membrosequipe.equipe_idEquipe = '"+idEquipe+"'";   
-  console.log("EquipeDAO:obterMembrosEquipeTutor - sqlObterMembrosEquipeTutor = "+sqlObterMembrosEquipeTutor);
-  this._connection.query(sqlObterMembrosEquipeTutor, callback);
+EquipeDAO.prototype.obtermembrosequipeTutor = function(idEquipe, callback){
+  console.log("EquipeDAO:obtermembrosequipeTutor - INICIO");
+  var sqlObtermembrosequipeTutor = "SELECT * FROM conta_usuario INNER JOIN membrosequipe ON conta_usuario.idContaUsuario = membrosequipe.conta_usuario_idContaUsuario INNER JOIN equipe ON membrosequipe.equipe_idEquipe = equipe.idEquipe WHERE membrosequipe.equipe_idEquipe = '"+idEquipe+"'";   
+  console.log("EquipeDAO:obtermembrosequipeTutor - sqlObtermembrosequipeTutor = "+sqlObtermembrosequipeTutor);
+  this._connection.query(sqlObtermembrosequipeTutor, callback);
 }
 
 EquipeDAO.prototype.obterTutorEquipe = function(idEquipe, callback){
   console.log("EquipeDAO:obterTutorEquipe - INICIO");
   var sqlObterTutor = "SELECT * FROM conta_usuario INNER JOIN equipe ON conta_usuario.idContaUsuario = equipe.idTutor WHERE equipe.idEquipe = "+idEquipe;
-  console.log("EquipeDAO:obterMembrosEquipe - sqlObterMembrosEquipe = "+sqlObterTutor);
+  console.log("EquipeDAO:obtermembrosequipe - sqlObtermembrosequipe = "+sqlObterTutor);
   this._connection.query(sqlObterTutor, callback);
 }
 EquipeDAO.prototype.verificarEquipeVinculadoTutor = function(idContaUsuario, callback){
